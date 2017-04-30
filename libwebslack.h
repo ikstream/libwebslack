c0e9d669cfd80e815edd315a4df785f8fd2e89b7
Stefan Venz <stefan.venz@protonmail.com>
2017-05-01 00:41:14 +0200

    Update header file
    Signed-off-by: Stefan Venz <stefan.venz@protonmail.com>

--- libwebslack.h
+++ libwebslack.h
@@ -23,48 +23,64 @@
 #define MAX_CHANNEL_LENGTH 22
 #define MAX_USER_LENGTH 21
 #define MAX_TEXT_LENGTH 1024
+#define MAX_EMOJI_LENGTH 512
 
 struct team_info {
 	char webhook_url[MAX_HOOK_LENGTH + 1];
 	char channel[MAX_CHANNEL_LENGTH + 1];
 	char username[MAX_USER_LENGTH + 1];
 	char text[MAX_TEXT_LENGTH + 1];
+	char emoji[MAX_EMOJI_LENGTH + 1];
 };
 
 /*
  * set webhook to use
- * @teaminfo: team struct
- * @webhook_url: webhook url of your team to use
+ * @param team_info: team struct
+ * @param webhook_url: webhook url of your team to use
  * return 0 on success, else 1
  */
-int set_webhook_url(struct team_info *team_info, char *webhook_url);
+int set_webhook_url(struct team_info *team_info, const char *webhook_url);
 
 /*
  * set channel to post to
  * channel names may only be 22 letters in length
- * @team_info: team struct
- * @channel: channel to post to
+ * @param team_info: team struct
+ * @param channel: channel to post to
  * return 0 on success, else 1
  */
-int set_channel(struct team_info *team_info, char *channel);
+int set_channel(struct team_info *team_info, const char *channel);
 
 /*
  * set the user name for a team struct
  * The username may only contain lowercase letters, numbers, hyphens,
  * periods and underscores
- * @team_info: team struct
- * @username: username to set
+ * @param team_info: team struct
+ * @param username: username to set
  * return 0 on success, else 1
  */
-int set_username(struct team_info *team_info, char *username);
+int set_username(struct team_info *team_info, const char *username);
+
+/*
+ * set message to send
+ * @param team_info: team to send to
+ * @param text: send this text
+ * return ???? TODO
+ */
+int set_message(struct team_info *team_info, const char *text);
+
+/*
+ * Set emoji icon
+ * @param team_info: team struct
+ * @param emoji: emoji name
+ */
+int set_emoji(struct team_info *team_info, const char *emoji);
 
 /*
  * send message to team
- * @team_info: team to send to
- * @text: send this text
- * return ???? TODO
+ * @param param: team_info team info struct
+ * @param return: 0 on success, else 1
  */
-int send_message(struct team_info *team_info, char *text);
+int send_message(struct team_info *team_info);
 
 #endif
 
